0edf7424109db1a7b2c924c1287574a5120d486a
Ricky <n01094968@gmail.com>
2019-11-18 21:27:07 -0500

    attempted recycle view

--- app/src/main/java/com/partscrib/partscribmanagementsystem/PartsActivity.java
+++ app/src/main/java/com/partscrib/partscribmanagementsystem/PartsActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.partscrib.partscribmanagementsystem.ui.main.MyPartsRecyclerViewAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.widget.TextView;
 
 public class PartsActivity extends AppCompatActivity {
+
+
 
     private TextView partName;
     private TextView partAvailability;
@@ -21,6 +26,8 @@ public class PartsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_parts);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,4 +39,10 @@ public class PartsActivity extends AppCompatActivity {
         });
     }
 
+
+
+
+
 }
+
+
