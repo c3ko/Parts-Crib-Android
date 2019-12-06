d3bed628bd0f365db0b062ac9763472e0fc6a5e2
Ricky <n01094968@gmail.com>
2019-12-05 19:54:07 -0500

    Adding map

--- app/src/main/java/com/partscrib/partscribmanagementsystem/ui/main/HelpFragment.java
+++ app/src/main/java/com/partscrib/partscribmanagementsystem/ui/main/HelpFragment.java
@@ -1,38 +1,72 @@
 package com.partscrib.partscribmanagementsystem.ui.main;
 
-import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.partscrib.partscribmanagementsystem.R;
 
-public class HelpFragment extends Fragment {
+public class HelpFragment extends Fragment implements OnMapReadyCallback {
 
-    private HelpViewModel mViewModel;
+    private GoogleMap mMap;
 
-    public static HelpFragment newInstance() {
+    //private HelpViewModel mViewModel;
+
+    /*public static HelpFragment newInstance() {
         return new HelpFragment();
     }
-
+    */
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.help_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.help_fragment, container, false);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+
+        return rootView;
+        //mViewModel = ViewModelProviders.of(this).get(HelpViewModel.class);
+
+
+
+
+
     }
 
+
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(HelpViewModel.class);
         // TODO: Use the ViewModel
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng Humber = new LatLng(43.724330436, -79.605497578);
+        mMap.addMarker(new MarkerOptions()
+                .position(Humber)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.humber))
+                .title("Welcome to Humber College"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Humber, (float) 6.0));
+        mMap.setMinZoomPreference(2.0f);
+        mMap.setMaxZoomPreference(30.0f);
+        mMap.setTrafficEnabled(true);
     }
 
 }
